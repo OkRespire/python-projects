122d6ed4ab771bf5132436736b5e97b3d27e608a
OkRespire <rohane04@gmail.com>
2024-08-18 12:19:57 +0100

    Timer fully added, however logic is broken - need to fix

---
+++ new.py
@@ -0,0 +1,33 @@
+import time
+
+
+WORK_TIME = 25
+SHORT_REST_TIME = 5
+LONG_REST_TIME = 15
+completed = 0
+isWork = True
+running = True
+workTime = WORK_TIME
+restTime = SHORT_REST_TIME
+
+while True:
+    if running:
+        if isWork:
+            workTime -= 1
+            if workTime == -1:
+                completed += 1
+                isWork = False
+                restTime = SHORT_REST_TIME if completed % 4 == 0 else LONG_REST_TIME
+                print("Time to Rest!")
+
+        else:
+            restTime -= 1
+            if restTime == -1:
+                isWork = True
+                print("Time to Work!")
+                workTime = WORK_TIME
+
+        minutes, seconds = divmod(workTime if isWork else restTime, 60)
+
+        print("{:02d}:{:02d}".format(minutes, seconds))
+        time.sleep(0.7)
--- pomWindow.py
+++ pomWindow.py
@@ -1,25 +1,26 @@
 # import related packages
-import time
+from tkinter import messagebox
 import tkinter as tk
-# TODO: Use classes
 
-
-WORK_TIME = 25 * 60
-SHORT_REST_TIME = 5 * 60
-LONG_REST_TIME = 15 * 60
+# TODO: USE THE RIGHT VALUES WHEN FINISHING
+WORK_TIME = 2
+SHORT_REST_TIME = 5
+LONG_REST_TIME = 15
 
 
 class PomWindow:
     def __init__(self):
         self.win = tk.Tk()
         self.win.title("Pomodoro Timer by Rohane")
-        self.win.geometry("200x100")
-        self.isWork, self.isLongRest = True, False
+        self.win.geometry("400x300")
+        self.isWork = (True,)
         self.workTime, self.restTime = WORK_TIME, SHORT_REST_TIME
         self.completed, self.running = 0, False
 
         self.titleLabel = tk.Label(self.win, text="Pomodoro Timer", font="Calibri 20")
         self.titleLabel.pack()
+        self.timerLabel = tk.Label(self.win, text="", font=("TkDefaultFont", 40))
+        self.timerLabel.pack(pady=40)
 
         self.startButton = tk.Button(
             self.win, text="Start", command=self.startTimer, state="normal"
@@ -31,32 +32,54 @@ class PomWindow:
         )
         self.stopButton.pack()
 
-        self.minuteEntry = tk.Entry(self.win)
-        self.minuteEntry.pack()
-        self.minuteVal = tk.StringVar()
-        self.minuteVal.set("25")
-        self.minuteEntry["textvariable"] = self.minuteVal
-
-        self.secondEntry = tk.Entry(self.win)
-        self.secondEntry.pack()
-        self.secondVal = tk.StringVar()
-        self.secondVal.set("00")
-        self.secondEntry["textvariable"] = self.secondVal
-
         self.win.mainloop()
 
     def startTimer(self):
         self.running = True
         self.startButton.config(state="disabled")
         self.stopButton.config(state="normal")
+        self.titleLabel.config(text="Time to work!" if self.isWork else "Time to rest!")
         self.updateTimer()
 
     def stopTimer(self):
+        self.startButton.config(state="normal")
+        self.stopButton.config(state="disabled")
+        self.titleLabel.config(text="Paused.")
         self.running = False
 
     def updateTimer(self):
         if self.running:
+            if self.isWork:
+                self.workTime -= 1
+                if self.workTime == 0:
+                    messagebox.showinfo(
+                        "Work Complete!",
+                        "Now is the time to have a short rest!"
+                        if self.completed % 4 == 0
+                        else "Now is the time to take a long rest!",
+                    )
+                    self.completed += 1
+                    self.isWork = False
+                    self.restTime = (
+                        SHORT_REST_TIME if self.completed % 4 != 0 else LONG_REST_TIME
+                    )
+                    self.titleLabel.config(text="Time to Rest!")
+
+            else:
+                self.restTime -= 1
+                if self.restTime == 0:
+                    self.isWork = True
+                    self.workTime = WORK_TIME
+                    messagebox.showinfo("Rest Complete!", "Time to work!")
+                    self.titleLabel.config("Time to Work!")
+
+            minutes, seconds = divmod(
+                self.workTime if self.isWork else self.restTime, 60
+            )
+
+            self.timerLabel.config(text="{:02d}:{:02d}".format(minutes, seconds))
+
             self.win.after(1000, self.updateTimer)
 
 
-window = PomWindow()
+PomWindow()
